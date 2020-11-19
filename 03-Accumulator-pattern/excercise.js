3bb26c307e787b593af72d5e43fba0cb8fbd3d6e
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-20 01:42:26 +0530

    Excercise completed 🎯

--- 03-Accumulator-pattern/excercise.js
+++ 03-Accumulator-pattern/excercise.js
@@ -9,40 +9,44 @@ for (let i = 0; i < numbers_1.length; i++) {
     const number = numbers_1[i];
     sum += number; // 3. Update the accumulator variable
 }
-
+numbers_1.forEach(num => sum += num)
 console.log(sum);
 
 // 2 - Re-write the for loop with a forEach
 
 let things = ['shoe', 'table', 'book', 'computer', 'phone'];
 let copiedThings = []; // 1. Initialize accumulator variable
-
+let newCopiedThings = []
 // 2. Iterate over an array
 for (let i = 0; i < things.length; i++) {
     const thing = things[i];
     copiedThings.push(thing); // 3. Update the accumulator variable
 }
 
+things.forEach(thing => newCopiedThings.push(thing))
 console.log(copiedThings);
+console.log(newCopiedThings);
 
 // 3 - Re-write the for loop with a forEach
 
 let numbers_2 = [1, 2, 3, 4, 5];
 let doubledNumbers = []; // 1. Initialize accumulator variable
-
+let newDoubled = []
 // 2. Iterate over an array
 for (let i = 0; i < numbers_2.length; i++) {
     const number = numbers_2[i];
     doubledNumbers.push(number * 2); // 3. Update the accumulator variable
 }
 
-console.log(doubledNumbers);
+numbers_2.forEach(num => newDoubled.push(num * 2))
 
+console.log(doubledNumbers);
+console.log(newDoubled);
 // 4 - Re-write the for loop with a forEach
 
 let all_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let evenNumbers = []; // 1. Initialize accumulator variable
-
+let newEvenNumbers = []
 // 2. Iterate over an array
 for (let i = 0; i < all_numbers.length; i++) {
     const number = all_numbers[i];
@@ -50,5 +54,8 @@ for (let i = 0; i < all_numbers.length; i++) {
         evenNumbers.push(number); // 3. Update the accumulator variable
     }
 }
-
+all_numbers.forEach(num => {
+    num % 2 === 0 ? newEvenNumbers.push(num) : null
+})
 console.log(evenNumbers);
+console.log(newEvenNumbers);
