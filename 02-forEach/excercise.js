78e4d3080f6c1506fa94f3123aa7972fac5c5935
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-20 01:09:46 +0530

    Excercise completed 🎯

--- 02-forEach/excercise.js
+++ 02-forEach/excercise.js
@@ -1,6 +1,4 @@
 // 01 - Convert the following for loop to a forEach call on the array
-
-// Create the array
 const flavors = ['chocolate', 'ginger', 'carrot', 'coffee', 'walnut', 'banana'];
 
 // Create the for loop
@@ -8,39 +6,38 @@ for (let i = 0; i < flavors.length; i++) {
     console.log('I like ' + flavors[i] + ' cake');
 }
 
-// 02 - Convert the following for loop to a forEach call on the array
+flavors.forEach(flavor => console.log(`I like ${flavor} cake`))
 
+// 02 - Convert the following for loop to a forEach call on the array
 const numbers = [2, 4, 6, 8];
 
 for (let i = 0; i < numbers.length; i++) {
     console.log('The number', numbers[i], 'is at index', i);
 }
+numbers.forEach((num, index) => {
+    console.log(`The number ${num} is at index ${index}`);
+})
 
 // 03 - Convert the following for loop to a forEach call on the array
-
 const evenNumbers = [2, 4, 6, 8, 10];
 
 for (let i = 0; i < evenNumbers.length; i++) {
     evenNumbers[i] = evenNumbers[i] * 2;
 }
 
+evenNumbers.forEach((num, index, array) => array[index] = num * 2)
+
 console.log(evenNumbers);
 
 // 04 - Log the name of each product to the page with a forEach call on the products array
 
-let products = [{
-    name: 'Running shoes',
-    price: 75
-}, {
-    name: 'Golf shoes',
-    price: 85
-}, {
-    name: 'Dress shoes',
-    price: 95
-}, {
-    name: 'Walking shoes',
-    price: 65
-}, {
-    name: 'Sandals',
-    price: 55
-}];
+let products = [
+    { name: 'Running shoes', price: 75 },
+    { name: 'Golf shoes', price: 85 },
+    { name: 'Dress shoes', price: 95 },
+    { name: 'Walking shoes', price: 65 },
+    { name: 'Sandals', price: 55 }
+];
+products.forEach(product => {
+    console.log(product.name);
+});
